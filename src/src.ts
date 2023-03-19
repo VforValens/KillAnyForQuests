890bd508725c0c1d1dd35001036763e9413da8a9
VforValens <8889280+VforValens@users.noreply.github.com>
2023-03-19 17:56:46 -0400

    Update src.ts

--- src/src.ts
+++ src/src.ts
@@ -7,14 +7,48 @@ class KillAny implements IPostDBLoadMod
 {
     public postDBLoad(container: DependencyContainer): void 
     {
-        // get database from server
+        // Get database from server
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
 
         // Get all the in-memory json found in /assets/database
         const tables = databaseServer.getTables();
 
-        // get globals settings and set flea market min level to be 1
-        tables
+        // Set the quests variable
+        const quests = tables.templates.quests;
+        
+        // Set the index variable
+        let index = 0;
+        index++; // return value of index and increment index by one
+
+        // Kill Any instead of Kill Savage or Kill PMC
+        for (const quest in quests)
+        {
+            if (quests[quest].type == "Elimination")
+            {
+                const kills = quests[quest].conditions.AvailableForFinish.find(x => x._props.counter.conditions[index]._parent["Kills"]);
+                let target = kills._props.target;
+                target = "KillAny";
+            }
+        }
+
+        /* Partial credit for wrong type of Kills
+        for (const quest in quests)
+        {
+            if (quests[quest].type == "Elimination")
+            {
+                const savage = quests[quest].conditions.AvailableForFinish.find(x => x._props.counter.conditions[index]._parent["Kills"]._props.target = "Savage");
+                const pmc = quests[quest].conditions.AvailableForFinish.find(x => x._props.counter.conditions[index]._parent["Kills"]._props.target = "AnyPmc")
+                if (savage)
+                {
+                    const scav = quests[quest].conditions.AvailableForFinish.find(x => x._props.counter.conditions[index]);
+                    scav.concat(..._parent = "Kills", )
+                }
+                else if (pmc)
+                {
+                    
+                }
+            }
+        }*/
     }
 }
 
